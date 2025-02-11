84eabc3f3c3684063cecdc4658d3ae77360bf1e9
Filicity SH <f.elena.sh@gmail.com>
2025-02-11 14:35:51 -0800

    Protecting a Route

--- app.js
+++ app.js
@@ -60,36 +60,29 @@ app.use(passport.session());
 
 
 
-// secret word handling
-//let secretWord = "syzygy";
-// app.get("/secretWord", (req, res) => {
-//     if (!req.session.secretWord) {
-//       req.session.secretWord = "syzygy";
-//     }
-//     res.render("secretWord", { secretWord: req.session.secretWord });
-//   });
-app.get("/secretWord", (req, res) => {
-  if (!req.session.secretWord) {
-    req.session.secretWord = "syzygy";
-  }
-  res.locals.info = req.flash("info");
-  res.locals.errors = req.flash("error");
-  res.render("secretWord", { secretWord: req.session.secretWord });
-});
+
+
+
+
+const secretWordRouter = require("./routes/secretWord");
+const auth = require("./middleware/auth");
+app.use("/secretWord", auth, secretWordRouter);
   // app.post("/secretWord", (req, res) => {
   //   req.session.secretWord = req.body.secretWord;
   //   res.redirect("/secretWord");
   // });
-  app.post("/secretWord", (req, res) => {
-    if (req.body.secretWord.toUpperCase()[0] == "P") {
-      req.flash("error", "That word won't work!");
-      req.flash("error", "You can't use words that start with p.");
-    } else {
-      req.session.secretWord = req.body.secretWord;
-      req.flash("info", "The secret word was changed.");
-    }
-    res.redirect("/secretWord");
-  });
+  
+  // app.post("/secretWord", (req, res) => {
+  //   if (req.body.secretWord.toUpperCase()[0] == "P") {
+  //     req.flash("error", "That word won't work!");
+  //     req.flash("error", "You can't use words that start with p.");
+  //   } else {
+  //     req.session.secretWord = req.body.secretWord;
+  //     req.flash("info", "The secret word was changed.");
+  //   }
+  //   res.redirect("/secretWord");
+  // });
+  
 
 app.use((req, res) => {
   res.status(404).send(`That page (${req.url}) was not found.`);
